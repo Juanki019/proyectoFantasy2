267d305f2dad27bc453a3ab01440569cc0fcfff0
Juanki019 <juanki.sgbh@gmail.com>
2024-05-31 19:50:17 +0200

    cambios en ia

--- app/static/js/prediccion.js
+++ app/static/js/prediccion.js
@@ -1,23 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
-    $('#target_select').change(function() {
-        var selectedTargets = ['Precio', 'Goles', 'Puntos']; // Array con los nombres de las variables objetivo
-        $('#trainResponse').empty(); // Limpiar cualquier mensaje anterior
+    var showPlayerInfoButton = document.getElementById('show_player_info');
+    var playerSelect = document.getElementById('player_select');
+    var playerInfoDiv = document.getElementById('player_info');
 
-        // Iterar sobre las variables objetivo y realizar una solicitud AJAX para cada una
-        selectedTargets.forEach(function(target) {
-            $.ajax({
-                url: '/train',
-                type: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify({ target_column: target }),  
-                success: function(response) {
-                    $('#trainResponse').append('<p>Modelo entrenado correctamente para ' + target + '</p>');
-                    mostrarPredicciones(target); // Llamar a la función para mostrar las predicciones
-                },
-                error: function() {
-                    $('#trainResponse').append('<p>Error en el entrenamiento del modelo para ' + target + '</p>');
-                }
-            });
+    showPlayerInfoButton.addEventListener('click', function() {
+        var selectedPlayer = playerSelect.value;
+        fetch('/player_info?player=' + encodeURIComponent(selectedPlayer))
+            .then(response => response.json())
+            .then(data => {
+
+                playerInfoDiv.innerHTML = '';
+
+                var playerInfo = document.createElement('p');
+                playerInfo.textContent = 'Nombre: ' + data.Nombre + ', Equipo: ' + data.Equipo + ', Precio: ' + data.Precio + ', Puntos: ' + data.Puntos + ', Goles: ' + data.Goles + ', Trjetas rojas: ' + data.Tarjetas_rojas + ', Tarjetas amarillas: ' + data.Tarjetas_amarillas + ', Antepenultima Jornada: ' + data.Antepenultima_Jornada + ', Penultima Jornada: ' + data.Penultima_Jornada + ', Ultima Jornada: ' + data.Ultima_Jornada + ', Tendencia: ' + data.Flecha;
+                playerInfoDiv.appendChild(playerInfo);
+            })
+            .catch(error => console.error('Error al obtener la información del jugador:', error));
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    $('#target_select').change(function() {
+        var selectedTarget = $(this).val();
+        $.ajax({
+            url: '/train',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({target_column: selectedTarget}),  
+            success: function(response) {
+                $('#trainResponse').html('Aplicación lista para predecir su dato');
+            },
+            error: function() {
+                $('#trainResponse').html('Error en el entrenamiento del modelo.');
+            }
         });
     });
 
@@ -28,40 +43,18 @@ document.addEventListener('DOMContentLoaded', function() {
             url: '/predict',
             type: 'POST',
             contentType: 'application/json',
-            data: JSON.stringify({ player_name: player, target_column: target }),
+            data: JSON.stringify({player_name: player, target_column: target}),
             success: function(response) {
                 if (response.prediction) {
-                    $('#prediction_result').html('Predicción para ' + response.player_name + ' --> ' + target + ': ' + response.prediction);
+                    $('#prediction_result').html('Prediccion para ' + response.player_name + ' --> '+ target + ': ' + response.prediction);
                 } else {
-                    $('#prediction_result').html('No hay predicción disponible para ' + response.player_name + ' y ' + target);
+                    $('#prediction_result').html('No prediction available.');
                 }
             },
             error: function() {
-                $('#prediction_result').html('Error al recuperar la predicción.');
+                $('#prediction_result').html('Error retrieving prediction.');
             }
         });
     });
-
-    // Función para mostrar las predicciones
-    function mostrarPredicciones(target) {
-        // Obtener el jugador seleccionado y la variable objetivo
-        var player = $('#player_select').val();  
-        // Realizar la solicitud AJAX para obtener la predicción para el jugador y la variable objetivo actual
-        $.ajax({
-            url: '/predict',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({ player_name: player, target_column: target }),
-            success: function(response) {
-                if (response.prediction) {
-                    $('#prediction_result').append('<p>Predicción para ' + response.player_name + ' --> ' + target + ': ' + response.prediction + '</p>');
-                } else {
-                    $('#prediction_result').append('<p>No hay predicción disponible para ' + response.player_name + ' y ' + target + '</p>');
-                }
-            },
-            error: function() {
-                $('#prediction_result').append('<p>Error al recuperar la predicción para ' + target + '</p>');
-            }
-        });
-    }
 });
+
